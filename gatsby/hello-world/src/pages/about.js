66e5121c9dfd900a36ad5d2c5bcfd2c550a625e4
Adam Gegotek <a.gegotek@tekmi.nl>
2018-11-13 09:27:37 +0100

    Two pages, Typography

---
+++ gatsby/hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import H1 from "../components/h1"
+
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <H1 headerText="About Gatsbyyyy"/>
+        <p>Such wow. Very React.</p>
+    </div>
+)
--- gatsby/hello-world/src/pages/index.js
+++ gatsby/hello-world/src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import H1 from "../components/h1";
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+
+export default () => (
+    <div style={{ color: 'purple', backgroundColor: 'transparent'}}>
+        <H1 headerText="Hello Worlddddd"/>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <Link to="/about/">About</Link>
+        <Link to="/typo/">Typography</Link>
+    </div>
+)
